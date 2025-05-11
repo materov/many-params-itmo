6c2b3510a930afe3caad40fafbb1bd3118e32a1c
Evgeny Materov <materov@gmail.com>
2025-05-11 17:53:01 +0700

    finally! ❤️

--- main.R
+++ main.R
@@ -4,8 +4,21 @@ all_species <- unique(palmerpenguins::penguins$species)
 
 for (selected_species in all_species) {
   quarto::quarto_render(
-   input = "report.qmd",
+   input = "recording.qmd",
+   # cache_refresh = TRUE,
    execute_params = list(species = selected_species),
    output_file = glue::glue("report_{selected_species}.html")
+  )
+  fs::dir_create(selected_species)
+  fs::dir_copy(
+    "recording_files",
+    fs::path(selected_species, "recording_files")
+  )
+  fs::file_move(
+    glue::glue("report_{selected_species}.html"),
+    fs::path(
+      selected_species,
+      glue::glue("report_{selected_species}.html")
+    )
   )
 }
